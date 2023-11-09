59198632251bdfd8a02b18364171218064dd1247
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-09 16:41:53 +0200

    Fix the rubocop erroes

--- Gemfile
+++ Gemfile
@@ -74,4 +74,4 @@ group :test do
   gem 'webdrivers'
 end
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:Name, :Bio, :Photo]) # Add :name if you want to allow "Name"
-    devise_parameter_sanitizer.permit(:account_update, keys: [:Name, :Bio, :Photo]) # Add :name if you want to allow "Name"
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[Name Bio Photo]) # Add :name if you want to allow "Name"
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[Name Bio Photo]) # Add :name if you want to allow "Name"
   end
 end
