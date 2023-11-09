7b956a3adb2916f334cf16eddef6c56d3e45e59c
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-09 08:12:46 +0200

    Fix rubocop issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @user= User.includes(posts: %i[comments likes]).find(params[:user_id])
+    @user = User.includes(posts: %i[comments likes]).find(params[:user_id])
     @posts = @user.posts
   end
 
