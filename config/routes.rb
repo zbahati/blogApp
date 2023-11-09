cb33af173246e92ddbe4fffd5f67a2c87d052cc7
zbahati <bahatizirimwabagabo@gmai.com>
2023-11-09 14:46:57 +0200

    Ft add-device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'comment/new'
   get 'comment/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
