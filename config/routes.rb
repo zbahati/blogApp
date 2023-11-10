e0b7cd50ab1dc45049c021ef2f4ca9fda5bcf5e3
Okari Nyandika <nyandika15@icloud.com>
2023-11-10 21:17:15 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -17,6 +16,4 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
-
-
 end
