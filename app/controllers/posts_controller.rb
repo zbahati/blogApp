cbc6edf1490d06b2c7908c1d6c3390f637d65ae7
Okari Nyandika <nyandika15@icloud.com>
2023-11-11 12:55:47 +0300

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,6 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
-  def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
-  end
-
   def new
     @user = current_user
     @post = @user.posts.build
